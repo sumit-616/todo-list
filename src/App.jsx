d6fe28432ef43489b1d12ac86906e2070c21456d
sumit-616 <sumit616794@gmail.com>
2025-05-29 13:04:29 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ManageTodo from "./components/ManageTodo";
+import { Toaster } from "sonner";
+
+function App() {
+  return (
+    <>
+      <Toaster />
+      <ManageTodo />
+    </>
+  );
+}
+
+export default App;
